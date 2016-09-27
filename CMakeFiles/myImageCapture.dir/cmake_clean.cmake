015beeb67bc90c0c2598e1cbb7b9a975946d1aa6
weilinwu <316086283@qq.com>
2016-09-27 13:49:45 +0000

    -----------------------------Environment setup by weilinwu------------------------------------

---
+++ CMakeFiles/myImageCapture.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myImageCapture.dir/cvCamp.cpp.o"
+  "myImageCapture.pdb"
+  "myImageCapture"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myImageCapture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
